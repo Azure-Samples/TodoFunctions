a00e338398846b792e63f6fda32edf736ca5db2a
Anthony Chu <antchu@microsoft.com>
2017-08-15 12:06:10 -0700

    Add a hack to ensure directory default files are served with a trailing slash

---
+++ ToDoFunctions/www/js/ensure-directory-trailing-slash.js
@@ -0,0 +1,8 @@
+﻿// Ensure root directory has trailing slash
+// currently we're not able to do this redirect in the StaticFileServer because the proxy strips off the trailing slash
+(function () {
+    var loc = window.location;
+    if (loc.pathname.match(/\/[^\/\.]+$/)) {
+        window.location.href = loc.pathname + '/' + loc.search + loc.hash;
+    }
+}());
