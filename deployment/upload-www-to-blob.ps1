0627568de46fb75688d2e68de7dc7575458e5c98
Anthony Chu <antchu@microsoft.com>
2018-02-07 21:15:55 -0800

    Update upload to blob storage script

--- deployment/upload-www-to-blob.ps1
+++ deployment/upload-www-to-blob.ps1
@@ -2,7 +2,7 @@ $ProgressPreference="SilentlyContinue"
 
 $destinationUri = "https://$($Env:STORAGE_HOSTNAME)/www"
 $destinationKey = $Env:STORAGE_KEY
-.\deployment\AzCopy\AzCopy.exe /Source:.\ToDoFunctions\www /Dest:$destinationUri /DestKey:$destinationKey /SetContentType /S /Y
+D:\devtools\AzCopy\AzCopy.exe /Source:.\ToDoFunctions\www /Dest:$destinationUri /DestKey:$destinationKey /SetContentType /S /Y
 if ($LastExitCode -ne 0) { throw "azcopy returned code $($LastExitCode)" }
 
 $storageContext = New-AzureStorageContext -ConnectionString $Env:STORAGE_CONNECTION -ErrorAction Stop
